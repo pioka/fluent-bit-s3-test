a055494de241b71f8869e5d5a7cfb088a33610eb
pioka <20146647+pioka@users.noreply.github.com>
2022-08-14 22:48:07 +0900

    1行あたり100bytesのログになるように修正

--- logger.sh
+++ logger.sh
@@ -3,7 +3,7 @@
 set -eu
 
 while true; do
-  echo "$(date -Iseconds)  $(head -c 48 /dev/urandom | base64)" | tee -a $1
+  echo "$(date -Iseconds)  $(head -c 54 /dev/urandom | base64)" | tee -a $1
   sleep 1
 done
 
